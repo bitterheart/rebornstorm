eaffc679a7c27957f5873648a7e053c1db07ade1
Emory Merryman <emory.merryman@gmail.com>
2014-12-06 10:59:14 -0500

    added

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+(function(){
+    module.exports = function(grunt) {
+        grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+        });
+        grunt.registerTask('default', []);
+    };
+}());
