d863c97b66891094f17e654c432d94f7acee3405
Emory Merryman <emory.merryman@gmail.com>
2014-12-06 19:06:36 -0500

    added

--- server/application.js
+++ server/application.js
@@ -18,5 +18,8 @@
     "use strict";
     var express = require("express");
     var app = express();
+    app.get("/", function(req, res) {
+        res.send("hello!");
+    });
     module.exports = app;
 }());
